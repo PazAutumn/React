5cf0617502d7be9c5f1eedba4a7b6b89df798372
IsmaryAlvarez <ismaryad@gmail.com>
2018-03-01 12:52:55 -0300

    subiendo archivo de react

---
+++ src/components/WeatherLocation/WeatherData/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import WeatherTemperature from './WeatherTemperature';
+import WeatherExtraInfo from './WeatherExtraInfo';
+import {CLOUD, CLOUDY, SUN, RAIN, SNOW, WINDY} from './../../../constant/weathers'
+
+
+const WeatherData = () => (
+    <div>
+        <WeatherTemperature temperature = {'holi'} weatherState = {RAIN}/>
+        <WeatherExtraInfo humidity = {80} wind = {'10m/s'}/>
+    </div>
+)
+
+export default WeatherData;
